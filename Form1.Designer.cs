1f348501395abc5928888669cec4ed2589dc1230
musfiqurcse <md.musfiqurcse@outlook.com>
2015-05-16 17:18:37 +0600

    Final Submission

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CityInformation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.aboutTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.countryTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cityNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cityDetailsListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.searchbyCountryRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchbyCityRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.saveButton);
+            this.groupBox1.Controls.Add(this.aboutTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.countryTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cityNameTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(129, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 198);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "City Information";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(213, 165);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // aboutTextBox
+            // 
+            this.aboutTextBox.Location = new System.Drawing.Point(135, 67);
+            this.aboutTextBox.Multiline = true;
+            this.aboutTextBox.Name = "aboutTextBox";
+            this.aboutTextBox.Size = new System.Drawing.Size(153, 50);
+            this.aboutTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "About";
+            // 
+            // countryTextBox
+            // 
+            this.countryTextBox.Location = new System.Drawing.Point(135, 139);
+            this.countryTextBox.Name = "countryTextBox";
+            this.countryTextBox.Size = new System.Drawing.Size(153, 20);
+            this.countryTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Country";
+            // 
+            // cityNameTextBox
+            // 
+            this.cityNameTextBox.Location = new System.Drawing.Point(135, 29);
+            this.cityNameTextBox.Name = "cityNameTextBox";
+            this.cityNameTextBox.Size = new System.Drawing.Size(153, 20);
+            this.cityNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cityDetailsListView);
+            this.groupBox2.Controls.Add(this.searchbyCountryRadioButton);
+            this.groupBox2.Controls.Add(this.searchbyCityRadioButton);
+            this.groupBox2.Controls.Add(this.searchButton);
+            this.groupBox2.Controls.Add(this.searchTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 241);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(631, 230);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // cityDetailsListView
+            // 
+            this.cityDetailsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.cityDetailsListView.GridLines = true;
+            this.cityDetailsListView.Location = new System.Drawing.Point(40, 65);
+            this.cityDetailsListView.Name = "cityDetailsListView";
+            this.cityDetailsListView.Size = new System.Drawing.Size(570, 147);
+            this.cityDetailsListView.TabIndex = 12;
+            this.cityDetailsListView.UseCompatibleStateImageBehavior = false;
+            this.cityDetailsListView.View = System.Windows.Forms.View.Details;
+            this.cityDetailsListView.DoubleClick += new System.EventHandler(this.cityDetailsListView_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Serial No.";
+            this.columnHeader1.Width = 102;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "City Name";
+            this.columnHeader2.Width = 146;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "About";
+            this.columnHeader3.Width = 193;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Country";
+            this.columnHeader4.Width = 124;
+            // 
+            // searchbyCountryRadioButton
+            // 
+            this.searchbyCountryRadioButton.AutoSize = true;
+            this.searchbyCountryRadioButton.Location = new System.Drawing.Point(65, 42);
+            this.searchbyCountryRadioButton.Name = "searchbyCountryRadioButton";
+            this.searchbyCountryRadioButton.Size = new System.Drawing.Size(112, 17);
+            this.searchbyCountryRadioButton.TabIndex = 1;
+            this.searchbyCountryRadioButton.TabStop = true;
+            this.searchbyCountryRadioButton.Text = "Search by Country";
+            this.searchbyCountryRadioButton.UseVisualStyleBackColor = true;
+            this.searchbyCountryRadioButton.CheckedChanged += new System.EventHandler(this.searchbyCountryRadioButton_CheckedChanged);
+            // 
+            // searchbyCityRadioButton
+            // 
+            this.searchbyCityRadioButton.AutoSize = true;
+            this.searchbyCityRadioButton.Location = new System.Drawing.Point(65, 19);
+            this.searchbyCityRadioButton.Name = "searchbyCityRadioButton";
+            this.searchbyCityRadioButton.Size = new System.Drawing.Size(93, 17);
+            this.searchbyCityRadioButton.TabIndex = 0;
+            this.searchbyCityRadioButton.TabStop = true;
+            this.searchbyCityRadioButton.Text = "Search by City";
+            this.searchbyCityRadioButton.UseVisualStyleBackColor = true;
+            this.searchbyCityRadioButton.CheckedChanged += new System.EventHandler(this.searchbyCityRadioButton_CheckedChanged);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(498, 22);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(209, 22);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(257, 20);
+            this.searchTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 483);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "City Information";
+            this.Load += new System.EventHandler(this.CityInformation_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox aboutTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox countryTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox cityNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView cityDetailsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.RadioButton searchbyCountryRadioButton;
+        private System.Windows.Forms.RadioButton searchbyCityRadioButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+    }
+}
+
